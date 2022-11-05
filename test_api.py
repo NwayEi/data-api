bbd14d7e7fb20fb64d1856a8804138bef7699cf1
orchid aung <orchid.lso@gmail.com>
2022-11-05 17:02:37 +0800

    test api file

--- test_api.py
+++ test_api.py
@@ -1,4 +1,9 @@
-# pylint: disable=missing-docstring,invalid-name
+import requests
 
-# TODO: paste the code from Kitt's instructions
-
+url = "https://weather.lewagon.com/geo/1.0/direct?q=Barcelona"
+response = requests.get(url).json()
+print(response)
+city = response[0]
+print(city)
+print(f"{city['name']}: ({city['lat']}, {city['lon']})")
+print(type(city['lat']))
